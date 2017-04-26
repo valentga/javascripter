3df879290dcb7beb7cc9309a9585d8ec346d432c
Gina Valentino <gvalentino@Ginas-MacBook-Pro.local>
2017-04-26 01:38:35 -0400

    added seconds to hours function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 	This function converts number of hours into an equivalent number of seconds. 
 	
 	@ param hours    The number of hours to convert into seconds.
-	@ return result  A string output that prints the equivalent number of seconds. 
+	@ return result  A string output of the equivalent number of seconds 
 	*/
 	function hoursToSeconds(hours)
 	{
@@ -31,8 +31,54 @@
 		return result; 
 	}
 
+	/*
+	This function converts a number of seconds into an equivalent number of hours minutes and seconds
+
+	@ param numberOfSeconds      The number of seconds to convert to hours
+	@ return result              A string output of the equivalent time in hours, minutes, and seconds 
+	*/
+	function secondsToHours(numberOfSeconds)
+	{
+		//declaration of constants
+		const NUMBER_OF_SECONDS_IN_MINUTE = 60; 
+		const NUMBER_OF_MINUTE_IN_HOUR = 60; 
+
+		//declaration of variables
+		var hours; 
+		var remainingMin; 
+		var minutes; 
+		var seconds; 
+		var result;
+
+		//If the number of hours is less than 0, set the result to the string Invalid time entry. 
+		//Else convert the number of hours into an equivalent number of seconds.
+		if(numberOfSeconds < 0 || numberOfSeconds === 0)
+		{
+			result = "Invalid time entry"; 
+		}
+		else
+		{
+			console.log(numberOfSeconds + " seconds will be converted into an equivalent time in hours."); 
+
+			//determines number of hours in certain number of seconds
+			hours = Math.floor(numberOfSeconds / (NUMBER_OF_SECONDS_IN_MINUTE * NUMBER_OF_MINUTE_IN_HOUR));  
+			//determines remaining value after converting seconds into hours
+			remainingMin = numberOfSeconds % (NUMBER_OF_SECONDS_IN_MINUTE * NUMBER_OF_MINUTE_IN_HOUR); 
+			//from remaining value, determines the number of minutes left
+			minutes = Math.floor(remainingMin / NUMBER_OF_SECONDS_IN_MINUTE); 
+			//from remaining value, determines the nukber of seconds left
+			seconds = remainingMin % NUMBER_OF_SECONDS_IN_MINUTE; 
+
+			result = numberOfSeconds + " seconds is equal to " + hours + " hour(s) " + minutes + " minutes and " + seconds + " seconds."
+		}
+
+		return result; 
+	}
+
 
 module.exports = 
 {
-	hoursToSeconds: hoursToSeconds, 
+	hoursToSeconds: hoursToSeconds,
+
+	secondsToHours: secondsToHours, 
 }
